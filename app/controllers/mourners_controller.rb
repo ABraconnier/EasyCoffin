387fea062c52a2e7975447083ff3eb9e1298dd2e
Antoine Braconnier <antoine.braconnier@hotmail.com>
2019-11-04 17:57:18 +0100

    show and index method for controler mourner

--- app/controllers/mourners_controller.rb
+++ app/controllers/mourners_controller.rb
@@ -3,9 +3,11 @@ class MournersController < ApplicationController
   skip_before_action :authenticate_mourner!, only: [:index, :show]
 
   def index
+    @mourners = Mourner.all
   end
 
   def show
+    @mourner.find(params[:id])
   end
 
   def edit
