fc4187b51be84e35ba90c361957ff19d4d1cfb86
Antoine Lequeux <lequeuxa@gmail.com>
2019-11-05 16:01:07 +0100

    with bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(user)
+    if current_mourner
+      edit_mourner_path(user)
+    elsif current_client
+      edit_mourner_path(user)
+    end # your path
+  end
 end
