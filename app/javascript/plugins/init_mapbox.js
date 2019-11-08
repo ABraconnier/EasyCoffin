15e0a6a76b32980f0bf776b030975f8968898626
Antoine Braconnier <antoine.braconnier@hotmail.com>
2019-11-08 15:03:25 +0100

    fixed map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,12 +2,6 @@ import mapboxgl from 'mapbox-gl';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-  const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  if (markers !== undefined) {
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 7, duration: 0 });
-};
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -15,24 +9,31 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/dark-v10'
     });
-  const markers = JSON.parse(mapElement.dataset.markers);
-  fitMapToMarkers(map, markers);
-  markers.forEach((marker) => {
+    const markers = JSON.parse(mapElement.dataset.markers);
+    fitMapToMarkers(map, markers);
+    markers.forEach((marker) => {
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+      new mapboxgl.Marker(element)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(map);
+    });
+  }
+};
 
-    const element = document.createElement('div');
-    element.className = 'marker';
-    element.style.backgroundImage = `url('${marker.image_url}')`;
-    element.style.backgroundSize = 'contain';
-    element.style.width = '25px';
-    element.style.height = '25px';
-    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-    new mapboxgl.Marker(element)
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup)
-      .addTo(map);
-  });
-}
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  if (markers !== undefined) {
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 7, duration: 0 });
+  };
 };
-};
+
 export { initMapbox };
 
